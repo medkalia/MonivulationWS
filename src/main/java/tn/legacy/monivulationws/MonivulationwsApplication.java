549fb4d015b246e1e6850e652bf776f973629854
Mohamed <mohamed.kalia@esprit.tn>
2018-01-20 13:55:47 +0100

    Initial commit

---
+++ src/main/java/tn/legacy/monivulationws/MonivulationwsApplication.java
@@ -0,0 +1,12 @@
+package tn.legacy.monivulationws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonivulationwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonivulationwsApplication.class, args);
+	}
+}
